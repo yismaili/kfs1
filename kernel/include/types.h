e6ed13a9216ae405b9ae1566aff3f4548fe348f9
younes ismaili <ismailiyounes20@gmail.com>
2025-06-02 13:13:18 +0100

    add types

---
+++ kernel/include/types.h
@@ -0,0 +1,45 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+typedef unsigned char  uint8_t;
+typedef unsigned short uint16_t;
+typedef unsigned int   uint32_t;
+typedef unsigned long  uint64_t;
+
+typedef signed char    int8_t;
+typedef signed short   int16_t;
+typedef signed int     int32_t;
+typedef signed long    int64_t;
+
+typedef uint32_t size_t;
+typedef int32_t  ssize_t;
+
+typedef enum {
+    FALSE = 0,
+    TRUE = 1
+} bool_t;
+
+#ifndef NULL
+#define NULL ((void*)0)
+#endif
+
+typedef enum { //VGA color constants
+    VGA_BLACK = 0,
+    VGA_BLUE = 1,
+    VGA_GREEN = 2,
+    VGA_CYAN = 3,
+    VGA_RED = 4,
+    VGA_MAGENTA = 5,
+    VGA_BROWN = 6,
+    VGA_LIGHT_GREY = 7,
+    VGA_DARK_GREY = 8,
+    VGA_LIGHT_BLUE = 9,
+    VGA_LIGHT_GREEN = 10,
+    VGA_LIGHT_CYAN = 11,
+    VGA_LIGHT_RED = 12,
+    VGA_LIGHT_MAGENTA = 13,
+    VGA_LIGHT_BROWN = 14,
+    VGA_WHITE = 15,
+} vga_color_t;
+
+#endif 
